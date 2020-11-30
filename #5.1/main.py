de288c8b43e074460cd9f34a431c7929aa4463b7
EnxGitHub <65719051+EnxGitHub@users.noreply.github.com>
2020-11-30 22:43:38 +0300

    Create main.py

---
+++ #5.1/main.py
@@ -0,0 +1,38 @@
+import telebot
+from telebot import types
+import time
+
+TOKEN = '1457645096:AAEA2EXmMz6o9ob4jxipmaeaHE9Cx1BHX1k'
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.inline_handler(lambda query: query.query == 'test')
+def test(inline_query):
+    r = types.InlineQueryResultArticle(
+        # The id of our inline result
+        id='1',
+        title='test',
+        input_message_content=types.InputTextMessageContent(
+            'I am a message'
+        )
+    )
+    bot.answer_inline_query(inline_query.id, [r])
+
+
+@bot.inline_handler(lambda query: query.query == 'image')
+def image(inline_query):
+    r = types.InlineQueryResultPhoto(
+        id='11',
+        photo_url='https://images.assettype.com/swarajya/2020-08/a46bd36a-e65b-4b0b-91d9-c0fcac98c518/telegram.jpg?w=1200&h=800',
+        thumb_url='https://images.assettype.com/swarajya/2020-08/a46bd36a-e65b-4b0b-91d9-c0fcac98c518/telegram.jpg?w=1200&h=800'
+    )
+
+    bot.answer_inline_query(inline_query.id, [r])
+
+
+while True:
+    try:
+        bot.polling(True)
+    except Exception as e:
+        print(e)
+        time.sleep(5)
